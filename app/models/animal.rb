eafa0f6606d749b9aaa14320a3b1ec789e757d65
wolfieorama <wachirawamunyi@gmail.com>
2016-01-04 23:53:07 +0300

    preping auto loading

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,6 +2,9 @@ class Animal < ActiveRecord::Base
   belongs_to :tribe
   self.inheritance_column = :race
 
+  #If you want to disable Single Table Inheritance or use the type column for something else,
+  #you can use self.inheritance_column = :fake_column.
+
   #below are the animals subclassing the animals the Animal model
   def self.races
     %w(Lion WildBoar Meerkat)
