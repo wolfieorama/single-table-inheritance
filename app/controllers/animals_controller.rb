8809b569a66741b3ad621f8bf77edb96b4641a19
wolfieorama <wachirawamunyi@gmail.com>
2016-01-05 15:16:24 +0300

    added private modules and before action for specific subclasses

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,6 +1,7 @@
 class AnimalsController < ApplicationController
+  before_action :set_race
   def index
-    @animals = Animal.all
+    @animals = race_class.all
   end
 
   def show
@@ -19,5 +20,24 @@ class AnimalsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_race
+    @race = race
+  end
+
+  def race
+    Animal.races.include?(params[:race]) ? params[:race] : "Animal"
+  end
+
+ #this is more secure than passing the data blindly from the user
+  def race_classes
+    ["Animal", "Lion", "Meerkat", "Wildboar"]
+  end
+
+  def race_class
+    race.constantize if race.in? race_classes
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :animals
+  resources :lions, controller: "animals", type: "Lion"
+  resources :meerkats, controller: "animals", type: "Meerkat"
+  resources :wildboars, controller: "animals", type: "Wildboar"
+
   root 'animals#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
