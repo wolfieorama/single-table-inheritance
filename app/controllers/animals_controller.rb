6ce64c140190ae3c2b71a1fca8034143e3e0e993
wolfieorama <wachirawamunyi@gmail.com>
2016-01-05 19:46:35 +0300

    views logic

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -9,21 +9,39 @@ class AnimalsController < ApplicationController
   end
 
   def new
+    @animal = race_class.new
   end
 
   def edit
   end
 
   def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      redirect_to @animal, notice: "#{race} was successfuly saved."
+    else
+      render action: "new"
+    end
   end
 
   def update
+    if @animal.update(animal_params)
+      redirect_to @animal, notice: "{race} was successfuly created."
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
+    @animal.destroy
+    redirect_to animals_url
   end
 
   private
+
+  def animal_params
+    params.require(race.underscore.to_sym).permit(:name, :race, :age)
+  end
 
   def set_animal
     @animal = race_class.find(params[:id])
