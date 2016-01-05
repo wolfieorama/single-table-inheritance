96cc506b772d8fd8938f54247f6c401bcc471694
wolfieorama <wachirawamunyi@gmail.com>
2016-01-05 13:09:33 +0300

    generate controller and related routes

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,23 @@
+class AnimalsController < ApplicationController
+  def index
+    @animals = Animal.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
